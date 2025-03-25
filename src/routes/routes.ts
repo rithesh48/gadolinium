c2decc4db0aaebcd364e96ed8e0fc5079816604e
rithesh48 <ritheshdj889@gmail.com>
2025-03-25 15:35:22 +0530

    Implement user retrieval and token middleware for enhanced authentication

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,23 +1,14 @@
 import { Hono } from "hono";
 import { authenticationRoutes } from "./authentication-routes";
-import { prismaClient } from "../extras/prisma";
-import jwt from "jsonwebtoken";
-import { jwtSecretKey } from "../../environment";
 import { usersRoutes } from "./users-routes";
+import { logger } from "hono/logger";
 
 export const allRoutes = new Hono();
 
-allRoutes.use(async (context, next) => {
-  console.log("HTTP METHOD", context.req.method);
-  console.log("URL", context.req.url);
-  console.log("HEADERS", context.req.header());
-
-  await next();
-});
+allRoutes.use(logger());
 
 allRoutes.route("/authentication", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
-
 
 allRoutes.get("/health", (context) => {
   return context.json(
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,40 +1,46 @@
-import { Hono } from "hono" ;
-export const usersRoutes = new Hono();
-import jwt from "jsonwebtoken";
-import { jwtSecretKey } from "../../environment";
+import { Hono } from "hono";
 import { prismaClient } from "../extras/prisma";
+import { tokenMiddleware } from "./middlewares/token-middleware";
+import { getMe } from "../contoller/user/users-controller";
+import { GetMeError } from "../contoller/user/users-types";
 
-usersRoutes.get(
-    "/",
-    async (context, next) => {
-      const token = context.req.header("token");
-  
-      if (!token) {
-        return context.json(
-          {
-            message: "Missing Token",
-          },
-          401
-        );
-      }
-  
-      try {
-        const verified = jwt.verify(token, jwtSecretKey);
-  
-        await next();
-      } catch (e) {
-        return context.json(
-          {
-            message: "Missing Token",
-          },
-          401
-        );
-      }
-    },
-    async (context) => {
-      const users = await prismaClient.user.findMany();
-  
-      return context.json(users, 200);
+export const usersRoutes = new Hono();
+
+usersRoutes.get("/me", tokenMiddleware, async (context) => {
+  const userId = context.get("userId");
+
+  try {
+    const user = await getMe({
+      userId,
+    });
+
+    return context.json(
+      {
+        data: user,
+      },
+      200
+    );
+  } catch (e) {
+    if (e === GetMeError.BAD_REQUEST) {
+      return context.json(
+        {
+          error: "User not found",
+        },
+        400
+      );
     }
-  );
-  
+
+    return context.json(
+      {
+        message: "Internal Server Error",
+      },
+      500
+    );
+  }
+});
+
+usersRoutes.get("", tokenMiddleware, async (context) => {
+  const users = await prismaClient.user.findMany();
+
+  return context.json(users, 200);
+});
